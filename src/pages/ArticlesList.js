c9352a16fc299cdced5a12379a4817c5e8e8be63
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-08-06 00:20:36 +0530

    MOD: update ArticleList component to display articles list

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import articleContent from './article-content';
 
 const ArticlesList = () => (
     <div>
         <h1>Articles</h1>
+        {
+            articleContent.map((article, key) => (
+                <Link
+                    key={key}
+                    to={`/article/${article.name}`}
+                    className="article-list-item"
+                >
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0, 150)}...</p>
+                </Link>
+            ))
+        }
     </div>
 );
 
